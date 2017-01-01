28ff6992b0e4a649235e3f8bf40b50ded6e07343
Brendan McDonald <Brendan McDonald>
2017-01-01 11:12:19 -0500

    fixed up from Mark's email

--- blog.php
+++ blog.php
@@ -9,23 +9,23 @@
    <script src="assets/js/main.js"></script>
    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
    <link href="https://fonts.googleapis.com/css?family=Martel+Sans:200,400" rel="stylesheet">
-   <link rel="stylesheet" href="assets/css/new.css">
+   <link rel="stylesheet" href="assets/css/index.css">
    <link rel="stylesheet" href="assets/css/components.css">
  </head>
  <body>
  <!-- <img src="assets/media/logo.svg" alt="" class="logo"> -->
  <div class="DOMcontainer">
  <section class="one">
-   <div class="logo"></div>
-   <div class="nav">
+   <div class="nav" id="nav">
+     <img src="assets/media/logo.png" alt="" class="logo" id="logo">
      <ul>
-       <li><a href="index.html" onClick="activeItem(this)" class="nav-item" id="home">HOME</a></li>
+       <li><a href="#" onClick="activeItem(this)" class="active nav-item" id="home">HOME</a></li>
        <li><a href="services.html" onClick="activeItem(this)" class="nav-item" id="services">SERVICES</a></li>
        <li><a href="about.html" onClick="activeItem(this)" class="nav-item" id="about">WHO WE ARE</a></li>
-       <li><a href="#" onClick="activeItem(this)" class="active nav-item" id="blog">BLOG</a></li>
+       <li><a href="blog.php" onClick="activeItem(this)" class="nav-item" id="blog">BLOG</a></li>
        <li><a href="contact.html"><button class="nav-btn">CONTACT</button></a></li>
        <li><a href="login.html"><button class="nav-btn">LOGIN</button></a></li>
-       <li><a href="#"><button class="nav-btn">ADV</button></a></li>
+       <li><a href="adv.pdf" target='_blank'><button class="nav-btn">ADV</button></a></li>
      </ul>
    </div>
    <!-- <script type="text/javascript">
@@ -36,13 +36,9 @@
    <div class="nav-container"></div>
    <div class="title-container">
      <div class="title">
-       Proactive planning, <br><br> Intelligent portfolio design
+      The blog is coming soon.
      </div>
      <hr class="short-rule-blue">
-     <div class="subtitle">
-       This is Layline Advisors.
-     </div>
-     <button class="white-blue-button">Learn More</button>
    </div>
  </section>
 </body>
